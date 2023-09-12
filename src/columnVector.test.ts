8144cbf9cd14bd82e329b3280308183a4dad4a1b
angusjoshi1 <s1712180@ed.ac.uk>
2023-09-12 18:35:27 +0100

    remove old matrix files

--- src/columnVector.test.ts
+++
@@ -1,9 +0,0 @@
-import {ColumnVector} from "./columnVector";
-
-describe('constructor', () => {
-  it('constructs a column vector correctly', () => {
-    const aVector = new ColumnVector([1, 2, 3]);
-
-    expect(aVector.rows).toEqual([[1], [2], [3]]);
-  })
-})
